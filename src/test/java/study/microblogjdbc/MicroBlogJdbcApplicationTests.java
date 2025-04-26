cace84f22b0cb60b6a7acdd711fa74bcbacf8ebf
koo995 <107671886+koo995@users.noreply.github.com>
2025-04-26 18:56:48 +0900

    initial commit

---
+++ src/test/java/study/microblogjdbc/MicroBlogJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package study.microblogjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBlogJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
